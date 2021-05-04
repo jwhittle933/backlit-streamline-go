56fc96a4456ca1e24818944f8835215c94e6b84b
Jonathan Whittle <jonathan.m.whittle@gmail.com>
2021-05-03 22:33:53 -0400

    mdhd box parsing

--- pkg/media/mp4/box/moov/trak/mdia/api.go
+++ pkg/media/mp4/box/moov/trak/mdia/api.go
@@ -2,20 +2,37 @@
 package mdia
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/jwhittle933/streamline/pkg/media/mp4/box"
 	"github.com/jwhittle933/streamline/pkg/media/mp4/box/base"
+	"github.com/jwhittle933/streamline/pkg/media/mp4/box/children"
+	"github.com/jwhittle933/streamline/pkg/media/mp4/box/moov/trak/mdia/hdlr"
+	"github.com/jwhittle933/streamline/pkg/media/mp4/box/moov/trak/mdia/mdhd"
+	"github.com/jwhittle933/streamline/pkg/media/mp4/box/moov/trak/mdia/minf"
+	"github.com/jwhittle933/streamline/pkg/media/mp4/box/scanner"
+	"io"
 )
 
 const (
 	MDIA string = "mdia"
 )
 
+var (
+	Children = children.Registry{
+		hdlr.HDLR: hdlr.New,
+		mdhd.MDHD: mdhd.New,
+		minf.MINF: minf.New,
+	}
+)
+
 type Box struct {
 	base.Box
+	Children []box.Boxed
 }
 
 func New(i *box.Info) box.Boxed {
-	return &Box{base.Box{BoxInfo: i}}
+	return &Box{base.Box{BoxInfo: i}, make([]box.Boxed, 0)}
 }
 
 func (Box) Type() string {
@@ -23,10 +40,31 @@ func (Box) Type() string {
 }
 
 func (b Box) String() string {
-	return b.Info().String()
+	s := fmt.Sprintf("%s, boxes=%d", b.Info().String(), len(b.Children))
+
+	for _, c := range b.Children {
+		s += fmt.Sprintf("\n----->%s", c.String())
+	}
+
+	return s
 }
 
 // Write satisfies the io.Writer interface
 func (b *Box) Write(src []byte) (int, error) {
+	s := scanner.New(bytes.NewReader(src))
+
+	for {
+		child, err := s.ScanFor(Children)
+		if err == io.EOF {
+			return len(src), nil
+		}
+
+		if err != nil || child == nil {
+			break
+		}
+
+		b.Children = append(b.Children, child)
+	}
+
 	return len(src), nil
 }
--- pkg/media/mp4/box/moov/trak/mdia/minf/api.go
+++ pkg/media/mp4/box/moov/trak/mdia/minf/api.go
@@ -1,14 +1,31 @@
 // Package minf (Media Information)
 package minf
 
+import (
+	"github.com/jwhittle933/streamline/pkg/media/mp4/box"
+	"github.com/jwhittle933/streamline/pkg/media/mp4/box/base"
+)
+
 const (
 	MINF string = "minf"
 )
 
 type Box struct {
-	//
+	base.Box
+}
+
+func New(i *box.Info) box.Boxed {
+	return &Box{base.Box{BoxInfo: i}}
 }
 
 func (Box) Type() string {
 	return MINF
+}
+
+func (b *Box) String() string {
+	return b.Info().String()
+}
+
+func (b *Box) Write(src []byte) (int, error) {
+	return len(src), nil
 }
